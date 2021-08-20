c7cce020a4c42afd019e1529ff15f456201ec2e0
taiyi369 <71235765+taiyi369@users.noreply.github.com>
2021-08-20 16:28:02 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href=''
+      href='https://github.com/taiyi369/nextjs-notion-starter-kit'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
